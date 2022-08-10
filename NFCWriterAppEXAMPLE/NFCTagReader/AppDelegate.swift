5a0f555e801afedaf72835a571a13cd75744a037
Yusuf Morsi <ymorsi7@gmail.com>
2022-08-10 01:54:59 -0700

    uploaded NFC reader and writer app EXAMPLES

---
+++ NFCWriterAppEXAMPLE/NFCTagReader/AppDelegate.swift
@@ -0,0 +1,64 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main application delegate
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func readerSession(_ session: NFCNDEFReaderSession, didDetect tags: [NFCNDEFTag]) {
+        if tags.count > 1 {
+            // Restart polling in 500 milliseconds.
+            let retryInterval = DispatchTimeInterval.milliseconds(500)
+            session.alertMessage = "More than 1 tag is detected. Please remove all tags and try again."
+            DispatchQueue.global().asyncAfter(deadline: .now() + retryInterval, execute: {
+                session.restartPolling()
+            })
+            return
+        }
+        
+        // Connect to the found tag and write an NDEF message to it.
+        let tag = tags.first!
+        session.connect(to: tag, completionHandler: { (error: Error?) in
+            if nil != error {
+                session.alertMessage = "Unable to connect to tag."
+                session.invalidate()
+                return
+            }
+            
+            tag.queryNDEFStatus(completionHandler: { (ndefStatus: NFCNDEFStatus, capacity: Int, error: Error?) in
+                guard error == nil else {
+                    session.alertMessage = "Unable to query the NDEF status of tag."
+                    session.invalidate()
+                    return
+                }
+
+                switch ndefStatus {
+                case .notSupported:
+                    session.alertMessage = "Tag is not NDEF compliant."
+                    session.invalidate()
+                case .readOnly:
+                    session.alertMessage = "Tag is read only."
+                    session.invalidate()
+                case .readWrite:
+                    tag.writeNDEF(self.message, completionHandler: { (error: Error?) in
+                        if nil != error {
+                            session.alertMessage = "Write NDEF message fail: \(error!)"
+                        } else {
+                            session.alertMessage = "Write NDEF message successful."
+                        }
+                        session.invalidate()
+                    })
+                @unknown default:
+                    session.alertMessage = "Unknown NDEF tag status."
+                    session.invalidate()
+                }
+            })
+        })
+    }
+}
